cd42b38af809fd9d9dc0132b1783c6eb4fdb52dd
Sangrok Han <zibets.han@gmail.com>
2022-12-26 22:53:26 +0900

    Request hello to spring
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 function App() {
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('/api/hello')
+      .then((response) => setHello(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
+          <div> from backend : {hello}</div>
         </a>
       </header>
     </div>
