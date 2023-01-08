fdb497cfe4e61a10966fcca4fffb5ed295c18b5c
Sangrok Han <zibets.han@gmail.com>
2023-01-08 16:51:57 +0900

    Add hello request
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       .get('/api/hello')
       .then((response) => setHello(response.data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [hello]);
 
   return (
     <div className='App'>
