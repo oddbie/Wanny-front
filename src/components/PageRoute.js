8d4c2798afebebe7a6bd46baaf2eb85be17ab789
Sangrok Han <zibets.han@gmail.com>
2023-02-27 20:35:24 +0900

    Update
    - Add login & sign up page
    - Todo list add func
    - Make font & mui framework
    
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

--- src/components/PageRoute.js
+++ src/components/PageRoute.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from './Header';
-import Hello from './Hello';
-import Todo from './Todo';
-import Footer from './Footer';
-import NotFound from './NotFound';
+import Header from '../Header';
+import Hello from '../Hello';
+import Todo from '../Todo';
+import Footer from '../Footer';
+import NotFound from '../NotFound';
+import Login from './Login';
+import Signup from './Signup';
 
 const PageRoute = () => {
   return (
@@ -15,6 +17,8 @@ const PageRoute = () => {
         <Routes>
           <Route path='/' element={<Hello />} />
           <Route path='/todo' element={<Todo />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
